5886c35600b5a2538d42847a1b740e7fdf7a2951
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-04-08 23:01:02 +0700

    commit 11: view home screen

---
+++ app/home/index.js
@@ -0,0 +1,200 @@
+import { View, Text, Pressable, ActivityIndicator } from 'react-native'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Feather, FontAwesome6, Ionicons } from '@expo/vector-icons';
+import { theme } from '../../constants/theme';
+import { StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { ScrollView, TextInput } from 'react-native';
+import Categories from '../../components/categories';
+import { apiCall } from '../../api';
+import ImageGrid from '../../components/imageGrid';
+import { debounce } from 'lodash';
+import FiltersModal from '../../components/filtersModal';
+import { useRouter } from 'expo-router';
+
+
+
+var page = 1;
+const HomeScreen = () => {
+    const { top } = useSafeAreaInsets();
+    const paddingTop = top > 0 ? top + 10 : 30;
+    const [search, setSearch] = useState('');
+    const [images, setImages] = useState([]);
+    const [filters, setFilters] = useState(null);
+    const [activeCategory, setActiveCategory] = useState(null);
+    const searchInputRef = useRef(null);
+    const modalRef = useRef(null);
+    const scrollRef = useRef(null);
+    const router = useRouter();
+    const [isEndReached, setIsEndReached] = useState(false);
+
+    // console.log('filters: ', filters);
+    return (
+        <View style={[styles.container, { paddingTop }]}>
+            {/* header */}
+            <View style={styles.header}>
+                <Pressable onPress={handleScrollUp}>
+                    <Text style={styles.title}>
+                        Pixels
+                    </Text>
+                </Pressable>
+                <Pressable onPress={openFiltersModal}>
+                    <FontAwesome6 name="bars-staggered" size={22} color={theme.colors.neutral(0.7)} />
+                </Pressable>
+            </View>
+            <ScrollView
+                onScroll={handleScroll}
+                scrollEventThrottle={5} // how often scroll event will fire while scrolling (in ms)
+                ref={scrollRef}
+                contentContainerStyle={{ gap: 15 }}
+            >
+                {/* search bar */}
+                <View style={styles.searchBar}>
+                    <View style={styles.searchIcon}>
+                        <Feather name='search' size={24} color={theme.colors.neutral(0.4)} />
+                    </View>
+                    <TextInput
+                        placeholder='Search for photos....'
+                        // value={search}
+                        ref={searchInputRef}
+                        onChangeText={handleTextDebounce}
+                        style={styles.searchInput}
+                    />
+                    {
+                        search && (
+                            <Pressable onPress={() => handleSearch("")} style={styles.closeIcon}>
+                                <Ionicons name='close' size={24} color={theme.colors.neutral(0.6)} />
+                            </Pressable>
+                        )
+                    }
+
+                </View>
+                {/* categories */}
+                <View style={styles.categories}>
+                    <Categories activeCategory={activeCategory} handleChangeCategory={handleChangeCategory} />
+                </View>
+                {/* filters */}
+                {
+                    filters && (
+                        <View>
+                            <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.filters}>
+                                {
+                                    Object.keys(filters).map((key, index) => {
+                                        return (
+                                            <View key={key} style={styles.filterItem}>
+                                                {
+                                                    key == 'colors' ? (
+                                                        <View style={{
+                                                            height: 20,
+                                                            width: 30,
+                                                            borderRadius: 7,
+                                                            backgroundColor: filters[key]
+                                                        }} />
+                                                    ) : (
+                                                        <Text style={styles.filterItemText}>{filters[key]}</Text>
+                                                    )
+                                                }
+                                                <Pressable style={styles.filterCloseIcon} onPress={() => clearThisFilter(key)}>
+                                                    <Ionicons name='close' size={14} color={theme.colors.neutral(0.9)} />
+                                                </Pressable>
+                                            </View>
+                                        )
+
+                                    })
+                                }
+                            </ScrollView>
+                        </View>
+                    )
+                }
+
+                {/* images masonry grid */}
+                <View>
+                    {
+                        images.length > 0 && <ImageGrid images={images} router={router} />
+                    }
+                </View>
+                {/* loading */}
+                <View style={{ marginBottom: 70, marginTop: images.length > 0 ? 10 : 70 }}>
+                    <ActivityIndicator size={'large'} />
+                </View>
+            </ScrollView >
+            {/* filter modal */}
+            < FiltersModal
+                modalRef={modalRef}
+                filters={filters}
+                setFilters={setFilters}
+                onClose={closeFiltersModal}
+                onApply={applyFilters}
+                onReset={resetFilters}
+            />
+        </View >
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 15
+    },
+    header: {
+        marginHorizontal: wp(4),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: hp(4),
+        fontWeight: theme.fontWeights.semibold,
+        color: theme.colors.neutral(0.9)
+    },
+    searchBar: {
+        marginHorizontal: wp(4),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: theme.colors.grayBG,
+        backgroundColor: theme.colors.white,
+        padding: 6,
+        paddingLeft: 10,
+        borderRadius: theme.radius.lg,
+    },
+    searchIcon: {
+        padding: 8
+    },
+    searchInput: {
+        flex: 1,
+        borderRadius: theme.radius.sm,
+        paddingVertical: 10,
+        fontSize: hp(1.8)
+    },
+    closeIcon: {
+        backgroundColor: theme.colors.neutral(0.1),
+        padding: 8,
+        borderRadius: theme.radius.sm
+    },
+    filters: {
+        paddingHorizontal: wp(4),
+        gap: 10
+    },
+    filterItem: {
+        backgroundColor: theme.colors.grayBG,
+        padding: 3,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: theme.radius.xs,
+        padding: 8,
+        gap: 10,
+        paddingHorizontal: 10,
+    },
+    filterItemText: {
+        fontSize: hp(1.9)
+    },
+    filterCloseIcon: {
+        backgroundColor: theme.colors.neutral(0.2),
+        padding: 4,
+        borderRadius: 7
+    }
+})
+
+export default HomeScreen
