50fa5db359b4a11b8a5dec5824c29f3dd42ee6c3
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-04-15 22:49:02 +0700

    commit 16: ImageScreen UI

---
+++ app/home/image.js
@@ -0,0 +1,135 @@
+import { View, Text, StyleSheet, Button, Platform, ActivityIndicator, Pressable, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { BlurView } from 'expo-blur'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { Image } from 'expo-image';
+import { theme } from '../../constants/theme';
+import { Octicons } from '@expo/vector-icons';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+import Toast from 'react-native-toast-message';
+import * as MediaLibrary from 'expo-media-library';
+
+
+const ImageScreen = () => {
+    const router = useRouter();
+    const item = useLocalSearchParams();
+    const [status, setStatus] = useState('loading');
+    let uri = item?.webformatURL;
+    const fileName = item?.previewURL?.split('/').pop();
+    const imageURL = uri;
+    const filePath = `${FileSystem.documentDirectory}${fileName}`
+
+    return (
+        <BlurView
+            style={styles.container}
+            tint='dark'
+            intensity={60}
+        >
+            <View style={getSize()}>
+                <View style={styles.loading}>
+                    {
+                        status == 'loading' && <ActivityIndicator size={"large"} color={"white"} />
+                    }
+                </View>
+                <Image
+                    transition={100}
+                    style={[styles.image, getSize()]}
+                    source={uri}
+                    onLoad={onLoad}
+                />
+            </View>
+            {/* <Button title='Back' onPress={() => router.back()} /> */}
+            <View style={styles.buttons}>
+                <Animated.View entering={FadeInDown.springify()}>
+                    <Pressable style={styles.button} onPress={() => router.back()}>
+                        <Octicons name='x' size={24} color='white' />
+                    </Pressable>
+                </Animated.View>
+                <Animated.View entering={FadeInDown.springify().delay(100)}>
+                    {
+                        status == 'downloading' ? (
+                            <View style={styles.button}>
+                                <ActivityIndicator size={'small'} color={'white'} />
+                            </View>
+                        ) : (
+                            <Pressable style={styles.button} onPress={handleDownloadImage}>
+                                <Octicons name='download' size={24} color='white' />
+                            </Pressable>
+                        )
+                    }
+
+                </Animated.View>
+                <Animated.View entering={FadeInDown.springify().delay(200)}>
+                    {
+                        status == 'sharing' ? (
+                            <View style={styles.button}>
+                                <ActivityIndicator size={'small'} color={'white'} />
+                            </View>
+                        ) : (
+                            <Pressable style={styles.button} onPress={handleShareImage}>
+                                <Octicons name='share' size={22} color='white' />
+                            </Pressable>
+                        )
+                    }
+
+                </Animated.View>
+            </View>
+            <Toast config={toastConfig} visibilityTime={2500} />
+        </BlurView >
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: wp(4),
+        backgroundColor: 'rgba(0,0,0, 0.5)'
+    },
+    image: {
+        borderRadius: theme.radius.lg,
+        borderWidth: 2,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        borderColor: 'rgba(255, 255, 255, 0.1)'
+    },
+    loading: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttons: {
+        marginTop: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 50,
+    },
+    button: {
+        height: hp(6),
+        width: hp(6),
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        borderRadius: theme.radius.lg,
+        borderCurve: 'continuous'
+    },
+    toast: {
+        padding: 15,
+        paddingHorizontal: 30,
+        borderRadius: theme.radius.xl,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.15)'
+    },
+    toastText: {
+        fontSize: hp(1.8),
+        fontWeight: theme.fontWeights.semibold,
+        color: theme.colors.white
+    }
+})
+
+export default ImageScreen
